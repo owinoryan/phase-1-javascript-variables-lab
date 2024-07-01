12c5e10e598da164715081b40059ccf949c5b7f8
ryanowino <owinoryan@gmail.com>
2024-07-01 11:04:06 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Code your solution in this file!
+const companyName = 'Scuber';
+
+
+
+let mostProfitableNeighborhood = 'Chelsea';
+console.log(mostProfitableNeighborhood()).to.equal('Chelsea');
+
+
+let companyCeo = 'Susan Smith';
+console.log(companyCeo()).to.equal('Susan Smith');
